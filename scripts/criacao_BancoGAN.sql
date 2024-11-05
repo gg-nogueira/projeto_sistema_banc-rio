e3f41dbc182bffc253a62fa74f14ce709e777dcf
Gabriel Nogueira <biel.aurichio@gmail.com>
2024-11-04 23:25:28 -0300

    changed SQL scripts from MySQL to PostgreSQL

--- scripts/criacao_BancoGAN.sql
+++ scripts/criacao_BancoGAN.sql
@@ -1,25 +1,23 @@
-CREATE DATABASE BancoGAN;
-USE BancoGAn;
-
 CREATE TABLE usuarios (
     cpf VARCHAR(11) PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     data_nascimento DATE NOT NULL,
     senha VARCHAR(6) NOT NULL,
     CONSTRAINT cpf_unique UNIQUE (cpf)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+);
 
 CREATE TABLE contas (
-    numero_conta INT PRIMARY KEY AUTO_INCREMENT,
+    numero_conta SERIAL PRIMARY KEY,
     cpf_usuario VARCHAR(11),
     saldo DECIMAL(10,2),
     CONSTRAINT contas_ibfk_1 FOREIGN KEY (cpf_usuario) REFERENCES usuarios (cpf)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+);
 
 CREATE TABLE historico (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo_transacao ENUM('Depósito', 'Saque'),
+    id SERIAL PRIMARY KEY,
+    numero_conta INT,
+    tipo_transacao VARCHAR(10) CHECK (tipo_transacao IN ('Depósito', 'Saque')),
     valor DECIMAL(10,2),
-	data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT historico_ibfk_1 FOREIGN KEY (numero_conta) REFERENCES contas(numero_conta
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT historico_ibfk_1 FOREIGN KEY (numero_conta) REFERENCES contas (numero_conta)
+);
